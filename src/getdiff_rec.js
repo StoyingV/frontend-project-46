3c658fe68d15b661f265b32e01770273e81aa038
Vitaly <vitport@hotmail.com>
2023-09-08 08:07:50 +0000

    Задал по умолчанию значение stylish

--- src/getdiff_rec.js
+++ src/getdiff_rec.js
@@ -1,7 +1,7 @@
 import { readFileFlat } from './utils.js';
 import formatter from '../formaters/index.js';
 
-export default (path1, path2, format) => {
+export default (path1, path2, format = 'stylish') => {
   const file1 = readFileFlat(path1);
   const file2 = readFileFlat(path2);
   return formatter(file1, file2, format);
